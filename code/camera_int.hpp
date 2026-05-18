72d485ba4c3d2dc8af297cb2133f60e1abf19380
EKaiZhao <ekaizhao@outlook.com>
2026-05-18 16:23:47 +0800

    main:梳理camera,尝试使用整数运算节省算力,未测试未使用

---
+++ code/camera_int.hpp
@@ -0,0 +1,15 @@
+#ifndef CAMERA_INT_HPP
+#define CAMERA_INT_HPP
+
+#include "zf_common_headfile.hpp"
+#include "../user/interaction.hpp"
+
+// 大津法二值化
+// 输入：UVC 灰度图指针
+// 输出：gray_image 原地变成 0 / 255 二值图
+void otsu_threshold(uint8 *gray_image);
+
+// 获取当前二值化阈值，方便显示调试
+uint8 camera_get_threshold(void);
+
+#endif // CAMERA_INT_HPP
